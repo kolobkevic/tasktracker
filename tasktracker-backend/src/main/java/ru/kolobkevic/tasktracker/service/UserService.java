7f998bc5d90cb926fb38ad67bee5914f24ce180c
Zaur Bogdaev <kolobkevic@gmail.com>
2024-07-17 02:35:24 +0300

    feat: add UserService with creating, loading and deleting features

---
+++ tasktracker-backend/src/main/java/ru/kolobkevic/tasktracker/service/UserService.java
@@ -0,0 +1,42 @@
+package ru.kolobkevic.tasktracker.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.kolobkevic.tasktracker.model.User;
+import ru.kolobkevic.tasktracker.repository.UserRepository;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = getUserByUsername(username);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER")));
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Unable to find user with username: " + username));
+    }
+
+    public void deleteUser(String username) {
+        userRepository.deleteByUsername(username);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}

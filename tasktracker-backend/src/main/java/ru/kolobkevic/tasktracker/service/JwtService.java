575383f6874d007f5b1a56ea20c564edc7f995d2
Zaur Bogdaev <kolobkevic@gmail.com>
2024-07-17 02:36:50 +0300

    feat: add JwtService

---
+++ tasktracker-backend/src/main/java/ru/kolobkevic/tasktracker/service/JwtService.java
@@ -0,0 +1,63 @@
+package ru.kolobkevic.tasktracker.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ru.kolobkevic.tasktracker.model.User;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.lifetime}")
+    private int lifetime;
+
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+
+        claims.put("username", user.getUsername());
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(user.getUsername())
+                .issuedAt((new Date(System.currentTimeMillis())))
+                .expiration(new Date(System.currentTimeMillis() + lifetime))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    private SecretKey getSignKey() {
+        byte[] secretBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(secretBytes);
+    }
+
+    public Claims getAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token).getPayload();
+    }
+
+    private <T> T getClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        String username = getClaim(token, Claims::getSubject);
+        return user.getUsername().equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getClaim(token, Claims::getExpiration).before(new Date());
+    }
+}

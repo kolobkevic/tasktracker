af473667348c803616d6e721388d2d92539b9525
Zaur Bogdaev <kolobkevic@gmail.com>
2024-07-17 02:36:03 +0300

    feat: add User model

---
+++ tasktracker-backend/src/main/java/ru/kolobkevic/tasktracker/model/User.java
@@ -0,0 +1,37 @@
+package ru.kolobkevic.tasktracker.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "firstname", nullable = false, length = 50)
+    private String firstname;
+
+    @Column(name = "lastname", nullable = false, length = 50)
+    private String lastname;
+
+    @Column(name = "password", nullable = false, length = 50)
+    private String password;
+
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+
+}

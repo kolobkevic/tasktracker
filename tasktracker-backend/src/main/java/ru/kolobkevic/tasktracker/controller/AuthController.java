16ef872cc6ec5939d40a5850d138155ffcf433af
Zaur Bogdaev <kolobkevic@gmail.com>
2024-07-21 08:57:50 +0300

    fix: change AuthController

--- tasktracker-backend/src/main/java/ru/kolobkevic/tasktracker/controller/AuthController.java
+++ tasktracker-backend/src/main/java/ru/kolobkevic/tasktracker/controller/AuthController.java
@@ -1,54 +1,30 @@
 package ru.kolobkevic.tasktracker.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.kolobkevic.tasktracker.dto.JwtAuthenticationResponse;
 import ru.kolobkevic.tasktracker.dto.SignUpRequest;
-import ru.kolobkevic.tasktracker.model.User;
 import ru.kolobkevic.tasktracker.service.AuthService;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
-    private static final String REGISTRATION_PAGE = "auth/registration";
-    private static final String LOGIN_PAGE = "auth/login";
-
-    @GetMapping("/login")
-    public String showLoginPage() {
-        return LOGIN_PAGE;
-    }
-
-    @GetMapping("/registration")
-    public String showRegistrationPage(Model model) {
-        model.addAttribute("user", new User());
-        return REGISTRATION_PAGE;
-    }
 
     @PostMapping("/registration")
-    public String register(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+    public ResponseEntity<?> register(@RequestBody SignUpRequest request, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return REGISTRATION_PAGE;
+            return ResponseEntity.badRequest().build();
         }
-
-        SignUpRequest request = new SignUpRequest(
-                user.getUsername(), user.getEmail(),
-                user.getPassword(), user.getFirstname(), user.getLastname());
-
-        authService.signUp(request);
-        return LOGIN_PAGE;
-    }
-
-    @GetMapping("/logout")
-    public String logout(Model model) {
-        model.addAttribute("user", new User());
-        return LOGIN_PAGE;
+        log.info(request.toString());
+        JwtAuthenticationResponse response = authService.signUp(request);
+        log.info(response.toString());
+        return ResponseEntity.ok(response);
     }
 }

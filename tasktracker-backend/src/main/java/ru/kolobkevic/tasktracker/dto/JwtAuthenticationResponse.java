e029f64648d33945c026655ef9a6c5e3d8bf9662
Zaur Bogdaev <kolobkevic@gmail.com>
2024-07-23 20:47:02 +0300

    fix: add getter and setter

--- tasktracker-backend/src/main/java/ru/kolobkevic/tasktracker/dto/JwtAuthenticationResponse.java
+++ tasktracker-backend/src/main/java/ru/kolobkevic/tasktracker/dto/JwtAuthenticationResponse.java
@@ -1,10 +1,14 @@
 package ru.kolobkevic.tasktracker.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class JwtAuthenticationResponse {
     private String token;
 }

207c73a3ed9cf7a323b1d2c73c307ad5de4e2b09
Zaur Bogdaev <kolobkevic@gmail.com>
2024-07-27 16:42:20 +0300

    feat: add TaskRequest dto

---
+++ tasktracker-backend/src/main/java/ru/kolobkevic/tasktracker/dto/TaskRequest.java
@@ -0,0 +1,17 @@
+package ru.kolobkevic.tasktracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskRequest {
+    private Long id;
+    private String head = "";
+    private String content = "";
+    private String status = "";
+}

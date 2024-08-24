ab1e9a29b77cafeda5d2116cbe29b566f51888d2
Zaur Bogdaev <kolobkevic@gmail.com>
2024-08-24 14:12:10 +0300

    feat: add modelmapper bean

--- tasktracker-backend/src/main/java/ru/kolobkevic/tasktracker/config/SecurityConfig.java
+++ tasktracker-backend/src/main/java/ru/kolobkevic/tasktracker/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.kolobkevic.tasktracker.config;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -64,7 +65,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
@@ -72,5 +73,10 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }

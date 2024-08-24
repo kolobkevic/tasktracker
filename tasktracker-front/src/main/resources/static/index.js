e1ec72470b65842c5ab405a1cc1b90f92eab003e
Zaur Bogdaev <kolobkevic@gmail.com>
2024-08-24 13:49:47 +0300

    feat: add task logic

--- tasktracker-front/src/main/resources/static/index.js
+++ tasktracker-front/src/main/resources/static/index.js
@@ -1,7 +1,8 @@
 const hostApi = 'http://localhost:8080';
 const registrationUrl = '/auth/registration';
 const authenticationUrl = '/auth/authentication';
-const logoutUrl = '/logout';
+const tasksUrl = '/api/v1/tasks';
+const bearerHeader = 'Bearer '
 
 let signUpForm = document.getElementById('signUpForm');
 let signInForm = document.getElementById('signInForm');
@@ -9,21 +10,37 @@ let signUpError = document.getElementById('signUpError');
 let signInError = document.getElementById('signInError');
 let usernameElem = document.getElementById('username');
 let signOutButton = document.getElementById('signOutButton');
-let user;
+let deleteForm = document.getElementById('deleteForm');
+let editForm = document.getElementById('editForm');
+let createForm = document.getElementById('createForm');
+let createButton = document.getElementById('createButton');
+let signInButton = document.getElementById('signInButton')
+let signUpButton = document.getElementById('signUpButton')
+let inWorkTaskContainer = document.getElementById('inWorkTasksContainer')
+let doneWorkTaskContainer = document.getElementById('doneTasksContainer')
+let deleteButton;
+let editButton;
 
 signUpForm.addEventListener('submit', submitForm);
 signInForm.addEventListener('submit', submitForm);
 signOutButton.addEventListener('click', logout);
+deleteForm.addEventListener('submit', deleteTask);
+editForm.addEventListener('submit', editTask);
+createForm.addEventListener('submit', createTask)
+
+$(document).ready(function () {
+    if (isUserLoggedIn()) {
+        showTasks()
+    } else {
+        showUnAuthorizeElements()
+    }
+})
 
 function submitForm(event, form) {
     event.preventDefault()
     form = event.currentTarget
-    let formData = new FormData(form)
-    let json = {};
-    formData.forEach(function (value, key) {
-        json[key] = value;
-    });
 
+    let json = getJsonFromForm(form);
     let url;
     let error;
     if (form === signUpForm) {
@@ -45,13 +62,14 @@ function submitForm(event, form) {
         timeout: 10000,
     })
         .done(function (response) {
-            user = getUsernameAndTokenFromJwt(response.token)
-            showUsername()
+            localStorage.webUser = JSON.stringify({
+                username: parseJwt(response.token).username,
+                token: response.token
+            })
 
-            console.log(user)
-            console.log(user.token)
-            console.log(user.username)
-            showUsername()
+            showTasks()
+            showAuthorizeElements()
+
             form.reset();
             $('#modalSignIn').modal('hide');
             $('#modalSignUp').modal('hide');
@@ -77,34 +95,207 @@ function parseJwt(token) {
     return JSON.parse(jsonPayload);
 }
 
-function getUsernameAndTokenFromJwt(token) {
-    user = {username: parseJwt(token).username, "token": token};
-    console.log("User:")
-    console.log(user.username)
-    console.log(user.token)
-    return user
-}
-
 function isUserLoggedIn() {
-    return user;
-}
-
-function showUsername() {
-    usernameElem.textContent = user.username
+    return !!localStorage.webUser;
 }
 
 function logout(event) {
-    user = {}
-    console.log("Trying to log out")
+    if (isUserLoggedIn()) {
+        console.log("Trying to log out")
+        localStorage.clear()
+        showUnAuthorizeElements()
+    } else {
+        console.log("No user is logged in")
+    }
+}
 
+function showTasks() {
     $.ajax({
-        url: hostApi + logoutUrl,
-        type: "post",
-        timeout: 10000,
+        url: hostApi + tasksUrl,
+        type: "get",
+        headers: {Authorization: bearerHeader + JSON.parse(localStorage.webUser).token}
     })
         .done(function (response) {
+            let tasks = response
             console.log(response)
-            console.log(user.username)
-            usernameElem.textContent = ""
+
+            const taskContainer = $("#inWorkTasksContainer").empty()
+            $.each(tasks, function (i, task) {
+                const card = createTaskCard(task)
+                taskContainer.append(card)
+            })
+
+            deleteButton = document.getElementsByClassName('btn btn-link delete-button')
+            $.each(deleteButton, function (i, btn) {
+                btn.addEventListener('click', fillCardModal);
+            })
+
+            editButton = document.getElementsByClassName('btn btn-link edit-button')
+            $.each(editButton, function (i, btn) {
+                btn.addEventListener('click', fillCardModal);
+            })
         })
+}
+
+function createTaskCard(task) {
+    const card = $('<div>').addClass('card');
+
+    const title = $('<div>').addClass('card_title').append($('<h5>').text(task.head));
+    const content = $('<div>').addClass('card_content').append($('<p>').text(task.content));
+    const status = $('<div>').addClass('card_status').append($('<p>').text(task.status));
+
+    const created = $('<div>').addClass('card_created');
+    created.append($('<p>').addClass('card_created_head').text('created:'));
+    created.append($('<p>').addClass('card_created_date').text(convertDateToString(task.createdAt)));
+
+    const buttons = $('<div>').addClass('card_buttons');
+    buttons.append($('<div>').addClass('btn btn-link delete-button')
+        .attr("data-bs-target", "#deleteModal").attr("data-bs-toggle", "modal")
+        .text('Delete'));
+    buttons.append($('<div>').addClass('btn btn-link edit-button')
+        .attr("data-bs-target", "#editModal").attr("data-bs-toggle", "modal")
+        .text('Edit'));
+
+    card.append($('<div>').addClass('taskId').text(task.id));
+    card.append(title);
+    card.append(content);
+    card.append(status);
+    card.append(created);
+    card.append(buttons);
+
+    return card;
+}
+
+function fillCardModal(event, btn) {
+    event.preventDefault()
+    btn = event.currentTarget.className;
+
+    const id = $(event.target).closest(".card").find(".taskId").text();
+    const head = $(event.target).closest(".card").find(".card_title h5").text();
+    const content = $(event.target).closest(".card").find(".card_content p").text();
+    const status = $(event.target).closest(".card").find(".card_status p").text();
+
+    $(".taskId input").attr("value", id);
+
+    if (btn.includes('edit')) {
+        $("#editModalHead").attr("value", head);
+        $("#editModalContent").text(content);
+        $("#editModalStatus").attr("value", status)
+    } else if (btn.includes('delete')) {
+        $("#deleteModalHead").attr("value", head);
+        $("#deleteModalContent").text(content);
+        $("#deleteModalStatus").attr("value", status)
+    }
+}
+
+function deleteTask(event) {
+    event.preventDefault()
+
+    let formData = new FormData(deleteForm)
+    let id = formData.get('id')
+
+    $.ajax({
+        url: hostApi + tasksUrl + `/${id}`,
+        type: "delete",
+        contentType: "application/json",
+        timeout: 10000,
+        headers: {Authorization: bearerHeader + JSON.parse(localStorage.webUser).token}
+    })
+        .done(function (response) {
+            showTasks()
+            $('#deleteModal').modal('hide');
+        })
+        .fail(function () {
+                console.log("Something goes wrong")
+            }
+        )
+}
+
+function editTask(event) {
+    event.preventDefault()
+    let json = getJsonFromForm(editForm)
+
+    $.ajax({
+        url: hostApi + tasksUrl + '/edit',
+        type: "put",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(json),
+        timeout: 10000,
+        headers: {Authorization: bearerHeader + JSON.parse(localStorage.webUser).token}
+    })
+        .done(function (response) {
+            showTasks()
+            $('#editModal').modal('hide');
+        })
+        .fail(function () {
+                console.log("Something goes wrong")
+            }
+        )
+}
+
+function createTask(event) {
+    event.preventDefault()
+    let json = getJsonFromForm(createForm)
+
+    $.ajax({
+        url: hostApi + tasksUrl + '/create',
+        type: "post",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(json),
+        timeout: 10000,
+        headers: {Authorization: bearerHeader + JSON.parse(localStorage.webUser).token}
+    })
+        .done(function (response) {
+            showTasks()
+            $('#createModal').modal('hide');
+        })
+        .fail(function () {
+                console.log("Something goes wrong")
+            }
+        )
+}
+
+function getJsonFromForm(form) {
+    let formData = new FormData(form)
+    let json = {};
+    formData.forEach(function (value, key) {
+        json[key] = value;
+    });
+
+    return json
+}
+
+function showAuthorizeElements() {
+    createButton.hidden = false
+    signOutButton.hidden = false
+    signInButton.hidden = true
+    signUpButton.hidden = true
+    usernameElem.textContent = JSON.parse(localStorage.webUser).username
+}
+function showUnAuthorizeElements() {
+    createButton.hidden = true
+    signOutButton.hidden = true
+    signInButton.hidden = false
+    signUpButton.hidden = false
+    inWorkTaskContainer.replaceChildren()
+    doneWorkTaskContainer.replaceChildren()
+    usernameElem.textContent = ""
+}
+
+function convertDateToString(date){
+    const currentTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+    const currentLocale = Intl.DateTimeFormat().resolvedOptions().locale
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        timeZone: currentTimeZone
+    };
+
+    return Intl.DateTimeFormat(currentLocale, options).format(new Date(date))
 }

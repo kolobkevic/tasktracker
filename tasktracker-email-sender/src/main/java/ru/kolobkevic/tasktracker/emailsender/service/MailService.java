5f224653e28e9c37f574f0993965f89defd5eafc
Zaur Bogdaev <kolobkevic@gmail.com>
2024-09-08 11:14:19 +0300

    fix: change MailService, delete template engine

--- tasktracker-email-sender/src/main/java/ru/kolobkevic/tasktracker/emailsender/service/MailService.java
+++ tasktracker-email-sender/src/main/java/ru/kolobkevic/tasktracker/emailsender/service/MailService.java
@@ -9,32 +9,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring6.SpringTemplateEngine;
-import ru.kolobkevic.tasktracker.emailsender.dto.UserTopicDto;
-
-import java.util.HashMap;
-import java.util.Map;
+import ru.kolobkevic.tasktracker.emailsender.dto.EmailSendingDto;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class MailService {
-    private static final String WELCOME_MAIL_TEMPLATE = "registration_email.html";
-    private static final String WELCOME_MAIL_SUBJECT = "Welcome to TaskTracker";
-
-    private final SpringTemplateEngine templateEngine;
-        private final JavaMailSender mailSender;
-
     @Value("${spring.mail.username}")
-    private String username;
+    private String fromUsername;
 
+    private final JavaMailSender mailSender;
 
-    public void sendMail(String to, String subject, String content) throws MessagingException {
+    private void sendMail(String to, String title, String content) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         message.setContent(content, "text/html; charset=utf-8");
-        message.setSubject(subject);
-        message.setFrom(username);
+        message.setSubject(title);
+        message.setFrom(fromUsername);
         message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
         log.info("Sending email to {}", to);
@@ -42,22 +32,7 @@ public class MailService {
         log.info("Email sent to {}", to);
     }
 
-    public void sendTemplateMail(String to, String subject, String template, Map<String, Object> variables)
-            throws MessagingException {
-
-        sendMail(to, subject, templateEngine.process(template, generateTemplateContext(variables)));
-    }
-
-    public void sendWelcomeMail(UserTopicDto user) throws MessagingException {
-        Map<String, Object> variables = new HashMap<>();
-        variables.put("fullName", user.getFirstname() + " " + user.getLastname());
-
-        sendTemplateMail(user.getEmail(), WELCOME_MAIL_SUBJECT, WELCOME_MAIL_TEMPLATE, variables);
-    }
-
-    private Context generateTemplateContext(Map<String, Object> variables) {
-        Context context = new Context();
-        context.setVariables(variables);
-        return context;
+    public void sendMail(EmailSendingDto emailSendingDto) throws MessagingException {
+        sendMail(emailSendingDto.getEmail(), emailSendingDto.getTitle(), emailSendingDto.getContent());
     }
 }
